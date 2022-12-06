ab6a42d363a34b65eedff9f30ea5bb12434f1e09
ITSC <ITSC@gmail.com>
2022-12-06 21:51:50 +0900

    커밋

---
+++ exam-99/exam-99.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NOWARNINGS
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LEN 10
+#define TRUE 1
+#define FASLE 0
+
+#define ADD 1
+#define REMOVE 2
+#define CHECK 3
+#define ORDER 4
+#define END 5
+
+#define AMERICANO 1
+#define CAFELATTE 2
+#define JUICE 3
+#define ICECREAM 4
+#define CANCEL 5
